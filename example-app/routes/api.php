b2fad6f1ec60f8a496892b3d10f9ceb127ccd97d
misaellima <misael.lima@gmail.com>
2023-08-17 15:27:37 -0300

    main

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\UsuariosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('usuarios',[UsuariosController::class],'index');
+Route::get('usuarios',[UsuariosController::class],"index");
 Route::get('usuarios',[UsuariosController::class],"post");
